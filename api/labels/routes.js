98eaa4ab563e9f2ccdf6aa9a62ef52a0af26d1e2
Vicky C <c.vicky1213@gmail.com>
2017-04-25 15:05:09 -0400

    all the CRUD commands and it works in Postman

---
+++ api/labels/routes.js
@@ -0,0 +1,82 @@
+'use strict';
+
+const express = require('express');
+const mongoose = require('mongoose');
+const Label = require('./model/Label');
+const router = express.Router();
+
+//POST request
+router.route('/') 
+	.post((req,res) => {
+		const label = new Label(req.body);
+		label.save((err, label) => {
+			if(err){
+				res.status(404).json(err);
+			}
+			res.json(label);
+		});
+	});
+
+//GET request for all the labels
+router.route('/')
+	.get((req, res) => {
+		Label.find({}, (err, labels) => {
+			if(err){
+				res.status(404).json(err);
+			}
+			res.json(labels);
+		});
+	});
+
+//GET request for a specific label
+router.route('/:id')
+	.get((req, res) =>{
+		const _id = req.params.id;
+		Label.findOne({_id}, (err,label) => {
+			if(err){
+				res.status(404).json(err);
+			}
+			if(!label){
+				res.status(404).json({message: 'Label not found'});
+			}
+			res.json(label);
+		});
+	});
+
+//PUT request
+router.route('/:id')
+	.put((req, res) => {
+		const _id = req.params.id;
+		Label.findOneAndUpdate(
+			{_id},
+			req.body,
+			{new:true},
+			(err,contact) => {
+				if(err){
+					res.status(400).json(err);
+				}
+				res.json(contact);
+			}
+		);
+	});
+	
+
+//DELETE request
+router.route('/:id')
+	.delete((req, res) =>{
+		const _id = req.params.id;
+		Label.findOneAndRemove( 
+			{_id}, 
+			(err, label) => {
+				if(err){
+					res.status(400).json(err);
+				}
+				if(!label){
+					res.status(404).json({message: 'Label not found'});
+				}
+				res.json({message: `Label ${label.label_name} with id ${label._id} deleted.`});
+		});
+	});
+
+
+module.exports = router;
